78c2952e2835e4add20bd364f532948cfac9e1b8
matzukowski <mzukowski.dev@gmail.com>
2024-08-24 16:13:23 +0200

    implemented basic driver resources initialization and cleanup created symbolic link and device object for W: drive (W for wfs-driver) W: drive visible via CMD and File Explorer and is removed after driver service is stopped

--- wfsDriver/WfsDriver.c
+++ wfsDriver/WfsDriver.c
@@ -3,17 +3,115 @@
 #include "WfsDriver.h"
 #include "Messages.h"
 
-//NTSTATUS UnloadDriver(PDRIVER_OBJECT pDriverObject);
+PFILE_SYSTEM_DATA GlobalFileSystemData;
+
+NTSTATUS YourCreateRoutine(PDEVICE_OBJECT DeviceObject, PIRP Irp);
+NTSTATUS YourCloseRoutine(PDEVICE_OBJECT DeviceObject, PIRP Irp);
+NTSTATUS YourReadRoutine(PDEVICE_OBJECT DeviceObject, PIRP Irp);
+NTSTATUS YourWriteRoutine(PDEVICE_OBJECT DeviceObject, PIRP Irp);
+NTSTATUS YourQueryInformationRoutine(PDEVICE_OBJECT DeviceObject, PIRP Irp);
+NTSTATUS YourDirectoryControlRoutine(PDEVICE_OBJECT DeviceObject, PIRP Irp);
 
 NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegistryPath) {
+	NTSTATUS status;
+
+	//pDriverObject->MajorFunction[IRP_MJ_CREATE] = YourCreateRoutine;
+	//pDriverObject->MajorFunction[IRP_MJ_CLOSE] = YourCloseRoutine;
+	//pDriverObject->MajorFunction[IRP_MJ_READ] = YourReadRoutine;
+	//pDriverObject->MajorFunction[IRP_MJ_WRITE] = YourWriteRoutine;
+	//pDriverObject->MajorFunction[IRP_MJ_QUERY_INFORMATION] = YourQueryInformationRoutine;
+	//pDriverObject->MajorFunction[IRP_MJ_DIRECTORY_CONTROL] = YourDirectoryControlRoutine;
+
 	pDriverObject->DriverUnload = UnloadDriver;
-	DebugMessage("Hello World!");
+
+	status = InitializeDriverResources();
+	if (!NT_SUCCESS(status)) {
+		CleanupDriverResources();
+		return status;
+	}
+
+	status = CreateDevice(pDriverObject);
+	if (!NT_SUCCESS(status)) {
+		CleanupDriverResources();
+		return status;
+	}
+
+	DebugMessage("DriverEntry - full success!");
 
 	return STATUS_SUCCESS;
 }
 
 NTSTATUS UnloadDriver(PDRIVER_OBJECT pDriverObject) {
-	DebugMessage("Msg from UnloadDriver!");
+	CleanupDriverResources();
+	DeleteDevice(pDriverObject);
+
+	DebugMessage("UnloadDriver unloaded driver!");
+	return STATUS_SUCCESS;
+}
+
+NTSTATUS InitializeDriverResources() {
+	GlobalFileSystemData = (PFILE_SYSTEM_DATA)ExAllocatePool2(
+		POOL_FLAG_NON_PAGED,  // use POOL_FLAG_PAGED for paged pool if needed (whatever it is)
+		sizeof(FILE_SYSTEM_DATA),
+		FILE_SYSTEM_TAG
+	);
+	if (GlobalFileSystemData == NULL) {
+		return STATUS_INSUFFICIENT_RESOURCES;
+	}
+
+	InitializeListHead(&GlobalFileSystemData->FileList);
+	KeInitializeEvent(&GlobalFileSystemData->Event, SynchronizationEvent, FALSE);
+
+	return STATUS_SUCCESS;
+}
+
+NTSTATUS CleanupDriverResources() {
+	if (GlobalFileSystemData != NULL) {
+		ExFreePoolWithTag(GlobalFileSystemData, FILE_SYSTEM_TAG);
+		GlobalFileSystemData = NULL;
+	}
+
+	return STATUS_SUCCESS;
+}
+
+NTSTATUS CreateDevice(PDRIVER_OBJECT DriverObject) {
+	NTSTATUS status;
+	PDEVICE_OBJECT deviceObject;
+	UNICODE_STRING deviceName;
+	UNICODE_STRING symbolicLinkName;
+
+	RtlInitUnicodeString(&deviceName, DEVICE_NAME);
+	status = IoCreateDevice(
+		DriverObject,
+		0,
+		&deviceName,
+		FILE_DEVICE_DISK,
+		FILE_DEVICE_SECURE_OPEN,
+		FALSE,
+		&deviceObject
+	);
+
+	if (!NT_SUCCESS(status)) {
+		return status;
+	}
+
+	RtlInitUnicodeString(&symbolicLinkName, SYMBOLIC_LINK_NAME);
+	status = IoCreateSymbolicLink(&symbolicLinkName, &deviceName);
+
+	if (!NT_SUCCESS(status)) {
+		IoDeleteDevice(deviceObject);
+		return status;
+	}
+
+	return STATUS_SUCCESS;
+}
+
+NTSTATUS DeleteDevice(PDRIVER_OBJECT pDriverObject) {
+	UNICODE_STRING symbolicLinkName;
+
+	RtlInitUnicodeString(&symbolicLinkName, SYMBOLIC_LINK_NAME);
+	IoDeleteSymbolicLink(&symbolicLinkName);
+	IoDeleteDevice(pDriverObject->DeviceObject);
 
 	return STATUS_SUCCESS;
 }
--- wfsDriver/WfsDriver.h
+++ wfsDriver/WfsDriver.h
@@ -2,4 +2,29 @@
 
 #include <ntifs.h>
 
+#define FILE_SYSTEM_TAG 'FSYS'
+#define FILE_CONTROL_BLOCK_TAG 'FCB '
+#define IO_PACKET_TAG 'IOPB'
+#define FILE_OPERATION_TAG 'FOPT'
+#define METADATA_TAG 'META'
+#define DRIVER_INTERNAL_TAG 'DRIV'
+#define SYNCHRONIZATION_TAG 'SYNC'
+
+#define DEVICE_NAME L"\\Device\\WFSDevice"
+#define SYMBOLIC_LINK_NAME L"\\DosDevices\\W:"
+
 NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegistryPath);
+NTSTATUS UnloadDriver(PDRIVER_OBJECT DriverObject);
+
+typedef struct _FILE_SYSTEM_DATA {
+    LIST_ENTRY FileList;
+    KEVENT Event;
+
+} FILE_SYSTEM_DATA, * PFILE_SYSTEM_DATA;
+
+extern PFILE_SYSTEM_DATA GlobalFileSystemData;
+
+NTSTATUS InitializeDriverResources();
+NTSTATUS CleanupDriverResources();
+NTSTATUS CreateDevice(PDRIVER_OBJECT DriverObject);
+NTSTATUS DeleteDevice(PDRIVER_OBJECT DriverObject);
